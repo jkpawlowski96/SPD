97e36824c7341945f3031941f78165ae91b46587
234780 <rzeszowskaola@gmail.com>
2019-03-10 23:18:06 +0100

    Johnson's rule

--- Lab1/job.py
+++ Lab1/job.py
@@ -21,21 +21,21 @@ class Job:
 
 def cmax(perm, jobs):
     # array of end times (i-1 job on j-1 machine), now filled with 0
-    C = [[0] * (jobs[0].size + 2) for i in range(len(perm) + 2)]
+    C = [[0] * (len(perm) + 2) for i in range(jobs[0].size + 2)]
 
     for j in range(1, jobs[0].size + 1):  # machine number
         for i in range(1, len(perm) + 1):  # job number
             C[j][i] = max(C[j][i - 1], C[j - 1][i]) + jobs[perm[i - 1]].time(j - 1)
-
     # end time (max of array C)
     Cmax = 0
-    for j in range(len(perm) + 1):
-        for i in range(len(perm) + 1):
+    for j in range(jobs[0].size+1):
+        for i in range(len(perm)+1):
             Cmax = max(Cmax, C[j][i])
 
     return Cmax
 
-def AlgJohn(self, jobs):
+#Johnson's rule (two machines)
+def AlgJohn2(jobs):
     #group 1 contains jobs which time(0)<time(1)
     #group 2 contains other jobs
     G1=[]
@@ -49,12 +49,33 @@ def AlgJohn(self, jobs):
             print("G2: %d"%i)
 
     if len(G1):
-        G1[0].sort(0) #sort by time on machine 0
+        G1[0].sort_asc(0) #sort by time on machine 0 (asc)
     if len(G2):
-        G2[0].sort(1) #sort by time on machine 1
+        G2[0].sort_desc(1) #sort by time on machine 1 (desc)
     G=G1+G2
     ind=[]
     #optimal order
     for i in range(len(G)):
         ind=ind.append(G[i][0])
     return ind
+
+#Johnson's rule (three machines)
+def AlgJohn3(jobs):
+    #makes two virtual machines (time on vm0=time(0) + time(1); time on vm1=time(1)+time(2))
+    virtual_jobs_list=[]
+    for i in range(jobs):
+        virtual_jobs_list.append(Job([jobs[i].time(0)+jobs[i].time(1),jobs[i].time(1)+jobs[i].time(2)]))
+    return AlgJohn2(virtual_jobs_list)
+
+#Johnson's rule (k machines), I'm not sure if it's correct...
+def AlgJohn(jobs):
+    if jobs[0].size==2:
+        return AlgJohn2(jobs)
+    else:
+        times=[]
+        virtual_jobs_list=[]
+        for i in range(jobs):
+            for j in range(jobs-1):
+                times.append(jobs[i].time(j)+jobs[i].time(j+1))
+                virtual_jobs_list.append(Job(times))
+        return AlgJohn(virtual_jobs_list)
--- Lab1/solution_total.py
+++ Lab1/solution_total.py
@@ -4,9 +4,11 @@ from itertools import *
 jobs_list=[]
 
 # some jobs
-jobs_list.append(Job([10, 3]))
+jobs_list.append(Job([3, 3]))
 jobs_list.append(Job([1 , 4]))
-jobs_list.append(Job([1 , 1]))
+jobs_list.append(Job([1 , 2]))
+#jobs_list.append(Job([5 , 1]))
+
 
 jobs_queue = range(np.shape(jobs_list)[0])  # [0, 1, 2, n]
 
@@ -17,5 +19,6 @@ i = 0
 for comb in jobs_perm:    # for all combinations
     print('Time of comb ', i, ':', cmax(comb, jobs_list))      #time of combination
     i=i+1
+
 
 #AlgJohn(jobs_list)
