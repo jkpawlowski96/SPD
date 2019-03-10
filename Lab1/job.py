a11589f5ec65308557a5f082b45c8c4f31edaf8b
jkpawlowski96 <jkpawlowski96@gmail.com>
2019-03-10 14:32:54 +0100

    permutacje ale coś źle liczy

--- Lab1/job.py
+++ Lab1/job.py
@@ -19,18 +19,20 @@ class Job:
             return None
 
 
-    def cmax(self, perm,jobs):
-        #array of end times (i-1 job on j-1 machine), now filled with 0
-        C = [[0] * (jobs[0].size+2) for i in range(len(perm)+2)]
+def cmax(perm, jobs):
+    # array of end times (i-1 job on j-1 machine), now filled with 0
+    C = [[0] * (jobs[0].size + 2) for i in range(len(perm) + 2)]
 
-        for j in range(1,jobs[0].size+1): #machine number
-            for i in range(1,len(perm)+1): #job number
-                C[j][i]=max(C[j][i-1], C[j-1][i])+jobs[i-1].time(j-1)
+    for j in range(1, jobs[0].size + 1):  # machine number
+        for i in range(1, len(perm) + 1):  # job number
+            C[j][i] = max(C[j][i - 1], C[j - 1][i]) + jobs[i - 1].time(j - 1)
 
-        #end time (max of array C)
-        Cmax=0
-        for j in range(len(perm)+1):
-            for i in range(len(perm)+1):
-                Cmax=max(Cmax, C[j][i])
+    # end time (max of array C)
+    Cmax = 0
+    for j in range(len(perm) + 1):
+        for i in range(len(perm) + 1):
+            Cmax = max(Cmax, C[j][i])
 
-        return Cmax
+    return Cmax
+
+
--- Lab1/solution_total.py
+++ Lab1/solution_total.py
@@ -1,35 +1,19 @@
-from job import Job
-def swap(a,b):
-    return b, a
+from job import *
+from itertools import *
 
-def perm(k,P): #k jest potrzebne do rekurencji, bo permutacje chyba najlatwiej przez rekurencje wyznaczyc
-    #moze jest jakas gotowa biblioteka? Nie chcialo mi sie szukac, ogolnie odpal to sobie, odkomentowujac
-    #kolejny przyklad (linie 21-25) i zobacz jak to wyglada, gdyby udalo sie wrzucic to do p, przeglad zupelny bylby ok
-    if k==1:
-        print(P) #umiesz jakos to wrzucic do p? :(
+jobs_list=[]
 
-    else: #jesli zostalo wiecej niz 1 element do pozamieniania
-        for i in range(0,k):
-            P[i],P[k-1]=swap(P[i],P[k-1]) #zamiana pierwszego z ostatnim
-            perm(k-1,P) #permutacja k-1 elementow (k-ty jest juz dobrze ustawiony)
-            P[i],P[k-1]=swap(P[i],P[k-1]) # i powrot do pierwotnego polozenia
+# some jobs
+jobs_list.append(Job([10, 3]))
+jobs_list.append(Job([1 , 4]))
+jobs_list.append(Job([1 , 1]))
 
-pr=[0,1,2] #przykladowa permutacja
-j=[]
+jobs_queue = range(np.shape(jobs_list)[0])  # [0, 1, 2, n]
 
-#jakis przyklad
-j.append(Job([2,3]))
-j.append(Job([1,2]))
-j.append(Job([3,1]))
-#j.append(Job([1,1]))
-#j.append(Job([2,1]))
+jobs_perm = list(permutations(jobs_queue)) # [[1,2,3],[2,3,1]...]
 
-p=[]
-P = [] #permutacja potrzebna do perm
-for i in range(0, len(j)): #powinna byc wypelniona liczbami, ktore chcemy pozamieniac, u nas indeksy zadan, czyli od 0 do ilosci zadan
-    P.append(i)
-
-perm(len(j),P) #i wywolanie
-
-for i in range(len(p)): #a tutaj, gdyby permutacje byly zapisane w p, to wyswietla czasy zakoczenia dla konkretnej kolejnosci zadan
-    print("%s ---> %d"%(str(p[i]),j[0].cmax(p[i],j)))
+print(jobs_perm)
+i = 0
+for comb in jobs_perm:    # for all combinations
+    print('Time of comb ', i, ':', cmax(comb, jobs_list))      #time of combination
+    i=i+1
