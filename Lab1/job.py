7ab34f16482c01ca8280f7736a9b089e9a066e5f
jkpawlowski96 <jkpawlowski96@gmail.com>
2019-03-11 13:04:16 +0100

    new cmax: sim_time_queue, sim_time

--- Lab1/job.py
+++ Lab1/job.py
@@ -34,6 +34,31 @@ def cmax(perm, jobs):
 
     return Cmax
 
+
+def sim_time_queue(queue, jobs=[Job]):
+    machines = np.shape(jobs[0].time_on_machine)[0] # number of machines
+    machines_diary = [0] * machines                 # array [t_1,t_2,t_machines] then job is done (new time)
+    time=0
+    machines_working = [0] * machines
+
+    for job in queue:
+        sim_time(jobs[job], machines_diary)
+
+    return machines_diary[machines-1] #the last done job
+
+
+def sim_time(job, machines_diary):
+    machines_list = range(0, np.shape(job.time_on_machine)[0])
+    time = 0
+
+    for machine in machines_list:
+        delay = machines_diary[machine] - time # job delay
+        if delay < 0:
+            delay = 0
+        time = time + delay + job.time(machine)   # clock time of job done
+        machines_diary[machine] = time         # new time in diary
+
+
 #Johnson's rule (two machines)
 def AlgJohn2(jobs):
     #group 1 contains jobs which time(0)<time(1)
--- Lab1/solution_total.py
+++ Lab1/solution_total.py
@@ -4,21 +4,26 @@ from itertools import *
 jobs_list=[]
 
 # some jobs
-jobs_list.append(Job([3, 3, 1]))
-jobs_list.append(Job([1 , 4, 2]))
+jobs_list.append(Job([1,1,3]))
+jobs_list.append(Job([3,2,1]))
 jobs_list.append(Job([1 , 2, 1]))
-#jobs_list.append(Job([5 , 1, 9]))
+jobs_list.append(Job([5 , 1, 9]))
+jobs_list.append(Job([12 , 2, 1]))
+jobs_list.append(Job([1 , 5, 5]))
 
 
 jobs_queue = range(np.shape(jobs_list)[0])  # [0, 1, 2, n]
 
 jobs_perm = list(permutations(jobs_queue)) # [[1,2,3],[2,3,1]...]
 
-print(jobs_perm)
 i = 0
 for comb in jobs_perm:    # for all combinations
-    print('Time of comb ', i, ':', cmax(comb, jobs_list))      #time of combination
+    print('Time of comb ', i, ':', sim_time_queue(comb, jobs_list))      #time of combination
     i=i+1
 
 
-print(AlgJohn(jobs_list))
+'''
+c=AlgJohn(jobs_list)
+print(c)
+print('Time of comb ', i, ':', cmax(c, jobs_list))
+'''
