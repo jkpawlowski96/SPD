58f4bfffe37231e912839ebaaa5c647aa0680a32
jkpawlowski96 <jkpawlowski96@gmail.com>
2019-03-13 12:43:46 +0100

    file loader

--- Lab1/job.py
+++ Lab1/job.py
@@ -1,7 +1,14 @@
 import numpy as np
 from operator import itemgetter, attrgetter, methodcaller
 
+
 class Job:
+    """
+    Jobs req times on every machine so:
+
+    time_on_machines=[t1,t2,..,tn] for n machines
+
+    """
     # Constructor of jobe
     def __init__(self, time_on_machine):
         #array of times per machine
@@ -20,92 +27,22 @@ class Job:
             return None
 
 
-def cmax(perm, jobs):
-    # array of end times (i-1 job on j-1 machine), now filled with 0
-    C = [[0] * (len(perm) + 2) for i in range(jobs[0].size + 2)]
+def jobs_load(file_path='./data.txt'):
+    """
+    Load jobs from file.txt in format:
+                    1 2 4
+                    4 6 8
+                    1 3 4
+    :param file_path: path to .txt file
+    :return: list(Job)
+    """
+    with open(file_path, 'r') as f:
+        jobs_list=[]
+        for line in f:
+            values=line.rstrip().split(' ')
+            values=[int(i) for i in values]
+            jobs_list.append(Job(values))
+            if 'str' in line:
+                break
+    return jobs_list
 
-    for j in range(1, jobs[0].size + 1):  # machine number
-        for i in range(1, len(perm) + 1):  # job number
-            C[j][i] = max(C[j][i - 1], C[j - 1][i]) + jobs[perm[i - 1]].time(j - 1)
-    # end time (max of array C)
-    Cmax = 0
-    for j in range(jobs[0].size+1):
-        for i in range(len(perm)+1):
-            Cmax = max(Cmax, C[j][i])
-
-    return Cmax
-
-
-def sim_time_queue(queue, jobs=[Job]):
-    machines = np.shape(jobs[0].time_on_machine)[0] # number of machines
-    machines_diary = [0] * machines                 # array [t_1,t_2,t_machines] then job is done (new time)
-    time=0
-    machines_working = [0] * machines
-
-    for job in queue:
-        sim_time(jobs[job], machines_diary)
-
-    return machines_diary[machines-1] #the last done job
-
-
-def sim_time(job, machines_diary):
-    machines_list = range(0, np.shape(job.time_on_machine)[0])
-    time = 0
-
-    for machine in machines_list:
-        delay = machines_diary[machine] - time # job delay
-        if delay < 0:
-            delay = 0
-        time = time + delay + job.time(machine)   # clock time of job done
-        machines_diary[machine] = time         # new time in diary
-
-
-#Johnson's rule (two machines)
-def AlgJohn2(jobs):
-    #group 1 contains jobs which time(0)<time(1)
-    #group 2 contains other jobs
-    G1=[]
-    G2=[]
-    for i in range(len(jobs)):
-        if jobs[i].time(0)<jobs[i].time(1):
-            G1.append((jobs[i].time(0), jobs[i].time(1), i))
-        else:
-            G2.append((jobs[i].time(1),jobs[i].time(0), i))
-
-    if len(G1):
-        G1.sort()
-    if len(G2):
-        G2.sort(reverse=True)
-
-    G=G1+G2
-    ind=[]
-    #optimal order
-    for i in range(len(G)):
-        ind.append(G[i][2])
-    return ind
-
-#Johnson's rule (three machines)
-def AlgJohn3(jobs):
-    #makes two virtual machines (time on vm0=time(0) + time(1); time on vm1=time(1)+time(2))
-    virtual_jobs_list=[]
-    for i in range(len(jobs)):
-        virtual_jobs_list.append(Job([jobs[i].time(0)+jobs[i].time(1),jobs[i].time(1)+jobs[i].time(2)]))
-    return AlgJohn2(virtual_jobs_list)
-
-#Johnson's rule (k machines), I'm not sure if it's correct...
-def AlgJohn(jobs):
-    if jobs[0].size==2:
-        return AlgJohn2(jobs)
-    else:
-        times=[]
-        virtual_jobs_list=[]
-        print(jobs[0].size)
-        for i in range(jobs[0].size):
-            for j in range(len(jobs)):
-                #times on virtual machines
-                #times.append(jobs[j].time(i)+jobs[j].time(i+1))
-                print('.')
-            print(times)
-            #virtual_jobs_list.append(Job(times))
-            times=[]
-       # return AlgJohn(virtual_jobs_list)
--- Lab1/solution_total.py
+++ Lab1/solution_total.py
@@ -1,16 +1,19 @@
-from job import *
 from itertools import *
+from algorithms import *
 
 jobs_list=[]
 
+
+jobs_list=jobs_load()
+
 # some jobs
-jobs_list.append(Job([5 , 1, 2]))
+'''jobs_list.append(Job([5 , 1, 2]))
 jobs_list.append(Job([1,1, 1]))
 jobs_list.append(Job([3,2, 2]))
 jobs_list.append(Job([1 , 2,1]))
 jobs_list.append(Job([12 , 2, 1]))
 jobs_list.append(Job([1 , 5, 5]))
-
+'''
 
 jobs_queue = range(np.shape(jobs_list)[0])  # [0, 1, 2, n]
 
@@ -21,13 +24,13 @@ Tmin=0
 i = 0
 for comb in jobs_perm:    # for all combinations
     if Tmin==0:
-        Tmin=sim_time_queue(comb, jobs_list)
+        Tmin=c_max(comb, jobs_list)
     else:
-        Tmin=min(Tmin, sim_time_queue(comb, jobs_list))
+        Tmin=min(Tmin, c_max(comb, jobs_list))
     #print('Time of comb sim_ ', i, ':', sim_time_queue(comb, jobs_list))      #time of combination
     #print('Time of comb cmax ', i, ':', cmax(comb, jobs_list))      #time of combination
     i=i+1
 
 
 print('cmax perm time: ',Tmin)
-print('Alg John, time: ',sim_time_queue(AlgJohn3(jobs_list), jobs_list))
+print('Alg John, time: ',c_max(AlgJohn3(jobs_list), jobs_list))
