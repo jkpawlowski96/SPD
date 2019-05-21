972f4eb8914e2521986519b2e77d6e13df9800d8
234780 <rzeszowskaola@gmail.com>
2019-05-21 16:12:19 +0200

    .

--- Lab5/carlier.py
+++ Lab5/carlier.py
@@ -5,36 +5,35 @@ from schrage import cmax
 def carlier_algorithm(jobs_list, UB):
     jobs_list = jobs_list.copy()
     pi, U = schrange_nlogn(jobs_list)
-    pi_prim = pi[:]
+    pi_prim=[]
 
     if U < UB:
         UB = U
-        pi_star = pi[:]
-    b = -1
-    a = -1
-    c = -1
+        pi_prim = pi[:]
+    ind_c=-1
 
-    tab=cmax_tab(jobs_list, pi) #tablica zakonczenia wykonywania (r) kolejnych zadan
-
+    tab=cmax_tab(jobs_list, pi) #tablica zakonczenia wykonywania (p) kolejnych zadan
+    tab_end=[]
+    for j in range(len(tab)):
+        tab_end.append(tab[j]+jobs_list[pi[j]].tail[0])
+    Cmax_val=max(tab_end)
+    
     #wyznaczenie b
-    i=0
-    for task in pi:
-        if tab[i]+jobs_list[task].tail[0]==U:
-            b=task
+    for i in range(len(pi)):
+        if Cmax_val==tab[i]+jobs_list[pi[i]].tail[0]:
+            b=pi[i]
             ind_b=i
-        i=i+1
 
-    #wyznaczenie a    
-    i=0
-    for task in pi:
+    #wyznaczenie a
+    ind_a=-1
+    for i in range(len(pi)):
         suma=0
-        if(i<=ind_b):
-            for k in range(i, ind_b+1):
-                suma=suma+jobs_list[pi[k]].body[0]
-            if jobs_list[task].head[0]+suma+jobs_list[b].tail[0]==U:
-                a=task
+        for s in range(i, ind_b+1):
+            suma=suma+jobs_list[pi[s]].body[0]
+        if Cmax_val==jobs_list[pi[i]].head[0]+suma+jobs_list[b].tail[0]:
+            if ind_a==-1:
+                a=pi[i]
                 ind_a=i
-        i=i+1
 
     #wyznaczenie c
     for i in range(ind_a, ind_b+1):
@@ -43,41 +42,41 @@ def carlier_algorithm(jobs_list, UB):
             ind_c=i
 
     #jesli nie znaleziono c
-    if c == -1:
-        return pi_star
+    if ind_c == -1:
+        return pi_prim
 
     #wyznaczenie bloku
     Kappa=[]
     for i in range(ind_c+1, ind_b+1):
-        Kappa.append(pi[i])
+        Kappa.append(i)
 
 
     r_list=[]
     p_list=[]
     q_list=[]
     for kappa in Kappa:
-        r_list.append(jobs_list[kappa].head[0])
-        p_list.append(jobs_list[kappa].body[0])
-        q_list.append(jobs_list[kappa].tail[0])
+        r_list.append(jobs_list[pi[kappa]].head[0])
+        p_list.append(jobs_list[pi[kappa]].body[0])
+        q_list.append(jobs_list[pi[kappa]].tail[0])
 
     r_Kappa=min(r_list)
     q_Kappa=min(q_list)
     p_Kappa=sum(p_list)
 
-    #lewy potomek - zmiana wartosci r zadania krytycznego
-    old_rc=jobs_list[c].head[0]
-    jobs_list[c].head[0]=max(jobs_list[c].head[0], r_Kappa+p_Kappa)
-    LB=schargepmtn_nlogn(jobs_list)
-    
     h_Kappa=r_Kappa+p_Kappa+q_Kappa
-
-    #Kappa u {c}
+        #Kappa u {c}
     r_Kappa_u=min(r_Kappa, jobs_list[c].head[0])
     q_Kappa_u=min(q_Kappa, jobs_list[c].tail[0])
     p_Kappa_u=p_Kappa+jobs_list[c].body[0]
     
     h_Kappa_u=r_Kappa_u+p_Kappa_u+q_Kappa_u
-    
+
+
+    #lewy potomek - zmiana wartosci r zadania krytycznego
+    old_rc=jobs_list[c].head[0]
+    jobs_list[c].head[0]=max(jobs_list[c].head[0], r_Kappa+p_Kappa)
+
+    LB=schargepmtn_nlogn(jobs_list)
     LB=max(h_Kappa, h_Kappa_u, LB)
     
     if LB<UB: #jesli jest sens...
@@ -89,14 +88,8 @@ def carlier_algorithm(jobs_list, UB):
     #prawy potomek - zmiana wartosci q zadania krytycznego
     old_qc=jobs_list[c].tail[0]
     jobs_list[c].tail[0]=max(jobs_list[c].tail[0], q_Kappa+p_Kappa)
-    LB=schargepmtn_nlogn(jobs_list)
-
-    #Kappa u {c}
-    r_Kappa_u=min(r_Kappa, jobs_list[c].head[0])
-    q_Kappa_u=min(q_Kappa, jobs_list[c].tail[0])
-    p_Kappa_u=p_Kappa+jobs_list[c].body[0]
-    h_Kappa_u=r_Kappa_u+p_Kappa_u+q_Kappa_u
     
+    LB=schargepmtn_nlogn(jobs_list)
     LB=max(h_Kappa, h_Kappa_u, LB)
 
     if LB<UB: #jesli jest sens...
@@ -104,7 +97,6 @@ def carlier_algorithm(jobs_list, UB):
 
     #powrot z wartoscia q
     jobs_list[c].tail[0]=old_qc
-
     return pi_prim
 
     
--- Lab5/solution.py
+++ Lab5/solution.py
@@ -2,7 +2,26 @@ from job import *
 from schrage import *
 from carlier import *
 
+
+nazwa='./dane_testowe_RPQ/ola2.txt.'
+jobs_list = jobs_load2(nazwa)
+pi, U = schrange_nlogn(jobs_list)
+print('Schrage ', U, '\n', pi)
+U= schargepmtn_nlogn(jobs_list)
+print('Schrage pmtn ', U)
+order = carlier_algorithm(jobs_list, 100000)
+
+tab=cmax_tab(jobs_list, order)
+tab_end=[]
+for j in range(len(tab)):
+    tab_end.append(tab[j]+jobs_list[order[j]].tail[0])
+data='ola.txt'
+if len(tab_end)>0:
+    print(data,max(tab_end), '   --->   ')
+
+'''
 wynik=[228, 3026, 3665, 3309, 3191, 3618, 3446, 3821, 3634]
+ok=0;
 for n in range(9):
     nazwa='./dane_testowe_RPQ/data00'+str(n)+'.txt.'
     jobs_list = jobs_load2(nazwa)
@@ -13,9 +32,12 @@ for n in range(9):
     for j in range(len(tab)):
         tab_end.append(tab[j]+jobs_list[order[j]].tail[0])
     data='data00'+str(n)+'.txt'
-    print(data,max(tab_end), '   --->   ', wynik[n])
-
-
+    if len(tab_end)>0:
+        print(data,max(tab_end), '   --->   ', wynik[n])
+        if max(tab_end)==wynik[n]:
+            ok+=1
+'''
+'''
 jobs_list = jobs_load('./dane_testowe_RPQ/in50.txt')
 order = carlier_algorithm(jobs_list, 100000)
 
@@ -24,6 +46,8 @@ tab_end=[]
 for j in range(len(tab)):
     tab_end.append(tab[j]+jobs_list[order[j]].tail[0])
 print('in50.txt',max(tab_end), '   --->   ', 1492)
+if max(tab_end)==1492:
+    ok+=1
 
 jobs_list = jobs_load('./dane_testowe_RPQ/in100.txt')
 order = carlier_algorithm(jobs_list, 100000)
@@ -33,6 +57,8 @@ tab_end=[]
 for j in range(len(tab)):
     tab_end.append(tab[j]+jobs_list[order[j]].tail[0])
 print('in100.txt',max(tab_end), '   --->   ', 3070)
+if max(tab_end)==3070:
+    ok+=1
 
 jobs_list = jobs_load('./dane_testowe_RPQ/in200.txt')
 order = carlier_algorithm(jobs_list, 100000)
@@ -42,7 +68,8 @@ tab_end=[]
 for j in range(len(tab)):
     tab_end.append(tab[j]+jobs_list[order[j]].tail[0])
 print('in200.txt',max(tab_end), '   --->   ', 6398)
-
-
-
+if max(tab_end)==6398:
+    ok+=1
+'''
+#print('\n Ilosc poprawnych: ', ok)
 
