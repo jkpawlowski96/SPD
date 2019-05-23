a9de5f6d16ae098c4040874112166355a9025e0f
234780 <rzeszowskaola@gmail.com>
2019-05-23 10:48:43 +0200

    a, b, c
    wstawiłam prawidłowe wartości a, b, c dla 1. węzła, jak uruchomisz solution.py to powinieneś się zorientować jak to wygląda!

--- Lab5/schrage.py
+++ Lab5/schrage.py
@@ -75,23 +75,22 @@ def schrange(jobs_list):
     order = []
 
     # Algorymt
-    t = min_list(R)
-    while len(Nn) > 0 or len(Ng) > 0:
-        while len(Nn) > 0 and min_list(R) <= t:
-            j = arg_r(min_list(R), Nn)[0]
-            R.remove(min_list(R))
+    t = min(R)
+    while (len(Nn) > 0 or len(Ng) > 0):
+        while (len(Nn) != 0 and min(R) <= t):
+            j= arg_r(min(R), Nn)[0]
+            R.remove(min(R))
             Nn.remove(j)
             Ng.append(j)
             Q.append(jobs_list[j].tail[0])
         if len(Ng) == 0:
-            t = min_list(R)
+            t = min(R)
         else:
-            j = arg_q(max_list(Q), Ng)[0]
-            Q.remove(max_list(Q))
+            j = arg_q(max(Q), Ng)[0]
+            Q.remove(max(Q))
             Ng.remove(j)
             order.append(j)
             t += jobs_list[j].body[0]
-
     # S czas rozpoczecia zadania
     first = True
     for i in order:
@@ -116,6 +115,66 @@ def schrange(jobs_list):
 
     return order, cmax
 
+def schargepmtn(jobs_list):
+
+    def arg_r(v, jobs):
+        l = []
+        for i in jobs:
+            if v == jobs_list[i].head[0]:
+                l.append(i)
+        return l
+
+    def arg_q(v, jobs):
+        l = []
+        for i in jobs:
+            if v == jobs_list[i].tail[0]:
+                l.append(i)
+        return l
+
+    R = []
+    Q = []
+    for job in jobs_list:
+        R.append(job.head[0])
+
+    #S = np.zeros(len(jobs_list))  # momenty rozpoczęcia wykonywania zadań
+    #C = np.zeros(len(jobs_list))  # momenty zakończenia wykonywania zadań
+
+    Nn = list(range(len(jobs_list)))
+    Ng = []
+
+    #order = []
+
+    # Algorymt
+    t = 0
+    l=0
+    Cmax=0
+    while len(Nn) > 0 or len(Ng) > 0:
+        while len(Nn) > 0 and min(R) <= t:
+            j= arg_r(min(R), Nn)[0]
+            R.remove(min(R))
+            Nn.remove(j)
+            Ng.append(j)
+            Q.append(jobs_list[j].tail[0])
+            
+            if jobs_list[j].tail[0]>jobs_list[l].tail[0]:
+                jobs_list[l].body[0]=t-jobs_list[j].head[0]
+                t=jobs_list[j].head[0]
+
+                if jobs_list[l].body[0]>0:
+                    Ng.append(l)
+                    Q.append(jobs_list[l].tail[0])
+                    
+        if len(Ng) == 0:
+            t = min(R)
+        else:
+            j = arg_q(max(Q), Ng)[0]
+            Q.remove(max(Q))
+            Ng.remove(j)
+            l=j
+            t=t+jobs_list[j].body[0]
+            Cmax=max(Cmax, t+jobs_list[j].tail[0])
+
+    return Cmax
 
 def schrange_nlogn(jobs_list):
     '''
@@ -181,67 +240,6 @@ def schrange_nlogn(jobs_list):
     return order, cmax
 
 
-def schargepmtn(jobs_list):
-
-    def arg_r(v,jobs):
-        l = []
-        for i in jobs:
-            if v == jobs_list[i].head[0]:
-                l.append(i)
-        return l
-
-    def arg_q(v,jobs):
-        l = []
-        for i in jobs:
-            if v == jobs_list[i].tail[0]:
-                l.append(i)
-        return l
-
-    R = []
-    Q = []
-    for job in jobs_list:
-        R.append(job.head[0])
-
-    #S = np.zeros(len(jobs_list))  # momenty rozpoczęcia wykonywania zadań
-    #C = np.zeros(len(jobs_list))  # momenty zakończenia wykonywania zadań
-
-    Nn = list(range(len(jobs_list)))
-    Ng = []
-
-    #order = []
-
-    # Algorymt
-    t = 0
-    l=0
-    Cmax=0
-    while len(Nn) > 0 or len(Ng) > 0:
-        while len(Nn) > 0 and min_list(R) <= t:
-            j = arg_r(min_list(R), Nn)[0]
-            R.remove(min_list(R))
-            Nn.remove(j)
-            Ng.append(j)
-            Q.append(jobs_list[j].tail[0])
-            
-            if jobs_list[j].tail[0]>jobs_list[l].tail[0]:
-                jobs_list[l].body[0]=t-jobs_list[j].head[0]
-                t=jobs_list[j].head[0]
-
-                if jobs_list[l].body[0]>0:
-                    Ng.append(l)
-                    Q.append(jobs_list[l].tail[0])
-                    
-        if len(Ng) == 0:
-            t = min_list(R)
-        else:
-            j = arg_q(max_list(Q), Ng)[0]
-            Q.remove(max_list(Q))
-            Ng.remove(j)
-            l=j
-            t=t+jobs_list[j].body[0]
-            Cmax=max(Cmax, t+jobs_list[j].tail[0])
-
-    return Cmax
-
 
 def schargepmtn_nlogn(jobs_list):
 
@@ -289,3 +287,13 @@ def schargepmtn_nlogn(jobs_list):
             Cmax = max(Cmax, t + jobs_list[j].tail[0])
 
     return Cmax
+
+
+
+
+
+
+
+
+
+            
--- Lab5/solution.py
+++ Lab5/solution.py
@@ -2,15 +2,19 @@ from job import *
 from schrage import *
 from carlier import *
 
-
+'''
 nazwa='./dane_testowe_RPQ/ola2.txt.'
+data='ola2.txt'
+print(data)
 jobs_list = jobs_load2(nazwa)
-pi, U = schrange_nlogn(jobs_list)
-print('Schrage ', U, '\n', pi)
-U= schargepmtn_nlogn(jobs_list)
-print('Schrage pmtn ', U)
-order = carlier_algorithm(jobs_list, 100000)
-
+pi, U = schrange(jobs_list)
+print('Schrage ', U)
+U= schargepmtn(jobs_list)
+print('Schrage pmtn ', U, '\n')
+order, UB = carlier_algorithm(jobs_list, 100000000)
+print('UB=', UB)
+print(order)
+print(pi)
 tab=cmax_tab(jobs_list, order)
 tab_end=[]
 for j in range(len(tab)):
@@ -19,57 +23,77 @@ data='ola.txt'
 if len(tab_end)>0:
     print(data,max(tab_end), '   --->   ')
 
+print('\n ----------------------------')
+'''
+nazwa='./dane_testowe_RPQ/ola.txt.'
+data='ola.txt'
+print(data, 'prawidlowe wartosci dla pierwszego wezla (a,b)=(4, 12), c=1',)
+jobs_list = jobs_load2(nazwa)
+pi, U = schrange(jobs_list)
+print('Schrage ', U)
+U= schargepmtn(jobs_list)
+print('Schrage pmtn ', U, '\n')
+order, UB = carlier_algorithm(jobs_list, 100000000)
+print('UB=', UB)
+print(order)
+print(pi)
+tab=cmax_tab(jobs_list, order)
+tab_end=[]
+for j in range(len(tab)):
+    tab_end.append(tab[j]+jobs_list[order[j]].tail[0])
+data='ola.txt'
+if len(tab_end)>0:
+    print(data,max(tab_end), '   --->   ')
 '''
 wynik=[228, 3026, 3665, 3309, 3191, 3618, 3446, 3821, 3634]
 ok=0;
 for n in range(9):
     nazwa='./dane_testowe_RPQ/data00'+str(n)+'.txt.'
     jobs_list = jobs_load2(nazwa)
-    order = carlier_algorithm(jobs_list, 100000)
+    osch, csch=schrange(jobs_list)
+    #print(osch, csch)
+    order, UB = carlier_algorithm(jobs_list, 100000)
 
-    tab=cmax_tab(jobs_list, order)
-    tab_end=[]
-    for j in range(len(tab)):
-        tab_end.append(tab[j]+jobs_list[order[j]].tail[0])
     data='data00'+str(n)+'.txt'
-    if len(tab_end)>0:
-        print(data,max(tab_end), '   --->   ', wynik[n])
-        if max(tab_end)==wynik[n]:
-            ok+=1
-'''
-'''
-jobs_list = jobs_load('./dane_testowe_RPQ/in50.txt')
-order = carlier_algorithm(jobs_list, 100000)
+    if UB==wynik[n]:
+        ok+=1
+        stat='OK'
+    else:
+        stat=''
 
-tab=cmax_tab(jobs_list, order)
-tab_end=[]
-for j in range(len(tab)):
-    tab_end.append(tab[j]+jobs_list[order[j]].tail[0])
-print('in50.txt',max(tab_end), '   --->   ', 1492)
-if max(tab_end)==1492:
+    print(data,UB, '   --->   ', wynik[n], '   ',stat)
+
+
+jobs_list = jobs_load('./dane_testowe_RPQ/in50.txt')
+order, UB = carlier_algorithm(jobs_list, 100000)
+
+if UB==1492:
     ok+=1
+    stat='OK'
+else:
+    stat=''
+print('in50.txt',UB, '   --->   ', 1492, '   ',stat)
 
 jobs_list = jobs_load('./dane_testowe_RPQ/in100.txt')
-order = carlier_algorithm(jobs_list, 100000)
+order,UB = carlier_algorithm(jobs_list, 100000)
 
-tab=cmax_tab(jobs_list, order)
-tab_end=[]
-for j in range(len(tab)):
-    tab_end.append(tab[j]+jobs_list[order[j]].tail[0])
-print('in100.txt',max(tab_end), '   --->   ', 3070)
-if max(tab_end)==3070:
+if UB==3070:
     ok+=1
+    stat='OK'
+else:
+    stat=''
+print('in100.txt',UB, '   --->   ', 3070, '   ',stat)
 
 jobs_list = jobs_load('./dane_testowe_RPQ/in200.txt')
-order = carlier_algorithm(jobs_list, 100000)
+order,UB = carlier_algorithm(jobs_list, 100000)
 
-tab=cmax_tab(jobs_list, order)
-tab_end=[]
-for j in range(len(tab)):
-    tab_end.append(tab[j]+jobs_list[order[j]].tail[0])
-print('in200.txt',max(tab_end), '   --->   ', 6398)
-if max(tab_end)==6398:
+if UB==6398:
     ok+=1
-'''
-#print('\n Ilosc poprawnych: ', ok)
+    stat='OK'
+else:
+    stat=''
+print('in200.txt',UB, '   --->   ', 6398, '   ',stat)
 
+print('\n Ilosc poprawnych: ', ok)
+
+'''
